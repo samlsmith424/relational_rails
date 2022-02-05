e0c51e41435a51a6a4c748f37159b62d8d268fa5
Sam Smith <samlsmith424@gmail.com>
2022-02-05 15:12:22 -0800

    Add more seed info

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,10 @@
 gym = Gym.create!(name: "24 Hour Fitness", city: "Los Angeles", capacity: 222, is_open: true)
 gym_2 = Gym.create!(name: "Equinox", city: "San Francisco", capacity: 250, is_open: true)
 gym_3 = Gym.create!(name: "Planet Fitness", city: "Antioch", capacity: 170, is_open: false)
+
 machine = gym.machines.create!(name: "Leg Press", section: "Resistance Machines", recommended_sets: 5, recommended_reps: 10, is_broken: false)
 machine_2 = gym.machines.create!(name: "Elliptical", section: "Cardio", recommended_sets: 1, recommended_reps: 1, is_broken: false)
 machine_3 = gym.machines.create!(name: "Smith Machine", section: "Personal Training", recommended_sets: 5, recommended_reps: 12, is_broken: true)
-machine_4 = gym.machines.create!(name: "Preacher Curl", section: "Resistance Machines", recommended_sets: 4, recommended_reps: 15, is_broken: true)
+machine_4 = gym_2.machines.create!(name: "Preacher Curl", section: "Resistance Machines", recommended_sets: 4, recommended_reps: 15, is_broken: true)
+machine_5 = gym_2.machines.create!(name: "Stair Climber", section: "Cardio", recommended_sets: 1, recommended_reps: 1000, is_broken: false)
+machine_6 = gym_3.machines.create!(name: "Hamstring Curl", section: "Resistance Machines", recommended_sets: 4, recommended_reps: 12, is_broken: false)
