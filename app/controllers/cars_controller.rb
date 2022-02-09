5592e43c821a830a3343f7c5f4e12d2e2ef7f5f3
Sully Birashk <sully.birashk@gmail.com>
2022-02-08 21:05:28 -0800

    Adding Delete Links on Parent and Child Show Pages

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -17,6 +17,12 @@ class CarsController < ApplicationController
     redirect_to "/cars/#{car.id}"
   end
 
+  def destroy
+    car = Car.find(params[:id])
+    car.destroy
+    redirect_to '/cars'
+  end
+
   def car_params
     params.permit(:name, :body_style, :mpg, :electrical)
   end
--- spec/features/cars/show_spec.rb
+++ spec/features/cars/show_spec.rb
@@ -10,4 +10,52 @@ RSpec.describe 'car show page' do
 
     expect(page).to have_content(car.name)
   end
+
+  it 'displays all cars in the cars index' do
+    dealership = Dealership.create!(name: "Honda", city: "Los Angeles", employees: 77, offer_financing: true)
+
+    car = dealership.cars.create!(name: "Civic", body_style: "Coupe", mpg: 55, electrical: true, dealership_id: 1)
+
+    visit "/cars"
+
+    expect(page).to have_content(car.name)
+  end
+
+  it 'can see a count of cars associated with each dealership' do
+    dealership = Dealership.create!(name: "Honda", city: "Los Angeles", employees: 77, offer_financing: true)
+
+    car = dealership.cars.create!(name: "Civic", body_style: "Coupe", mpg: 55, electrical: true, dealership_id: 1)
+
+    visit "/dealerships/#{dealership.id}"
+
+    expect(page).to have_content("Number of Cars: #{dealership.cars.count}")
+  end
+
+  it "Has a delete button" do
+    honda = Dealership.create!(name: 'Honda', city: 'Thornton', employees: 46, offer_financing: true)
+    civic = honda.cars.create!(name: 'Civic', body_style: 'Riverside', mpg: 77, electrical: true)
+    accord = honda.cars.create!(name: 'accord', body_style: 'Denver', mpg: 38, electrical: true)
+
+    visit "/cars/#{civic.id}"
+
+    expect(page).to have_content("Delete #{civic.name}")
+  end
+
+  it "Can delete a car" do
+    honda = Dealership.create!(name: 'Honda', city: 'Thornton', employees: 46, offer_financing: true)
+    civic = honda.cars.create!(name: 'Civic', body_style: 'Riverside', mpg: 77, electrical: true)
+    accord = honda.cars.create!(name: 'accord', body_style: 'Denver', mpg: 38, electrical: true)
+
+    visit "/cars/#{civic.id}"
+
+    expect(page).to have_content("Delete #{civic.name}")
+
+    expect(page).to have_content("Civic")
+
+    click_on("Delete #{civic.name}")
+
+    expect(page).to_not have_content("Civic")
+
+    expect(current_path).to eq( "/cars")
+  end
 end
